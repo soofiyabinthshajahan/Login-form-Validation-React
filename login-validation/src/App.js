a96197d702ae254a45bd1fdba3f9f93c39ac7415
Soofiya Binth Shajahan B <soofiyashajahan@gmail.com>
2023-08-18 14:17:22 +0530

    Made some minor updates

--- login-validation/src/App.js
+++ login-validation/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CreateAccount from './CreateAccount/CreateAccount';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Login from './Login-page/Login';
+import ForgotAccount from './ForgotAccount/ForgotAccount';
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
        
        <Route path='/' element={<CreateAccount/>}></Route>
        <Route path='Login-page' element={<Login/>}></Route>
+       <Route path='forgot-account' element={<ForgotAccount/>}></Route>
+       <Route path='create-account' element={<CreateAccount/>}></Route>
 
         </Routes>
       </div>
---
+++ login-validation/src/ForgotAccount/ForgotAccount.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../ForgotAccount/ForgotAccount.css'
+
+function ForgotAccount() {
+  return (
+    <div className='forgot-account'>
+      
+    </div>
+  )
+}
+
+export default ForgotAccount
